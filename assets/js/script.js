7f7db662171a5958ec105f778aa6d4d53ed78f89
jeisongarcia11 <74687446+jeisongarcia11@users.noreply.github.com>
2024-12-03 13:45:30 -0500

    Add-js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Funcion para abrir el menú hamburguesa
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
@@ -12,4 +13,21 @@ if(close){
     close.addEventListener('click', () =>{
         nav.classList.remove('active');
     })
+}
+
+//Funcion para seleccionar imagenes miniatura
+var MainImg = document.getElementById("MainImg");
+var smallimg =document.getElementsByClassName("small-img");
+
+smallimg[0].onclick = function(){
+    MainImg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function(){
+    MainImg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function(){
+    MainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function(){
+    MainImg.src = smallimg[3].src;
 }
